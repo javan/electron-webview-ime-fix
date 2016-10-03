704e4e83ab4bfa406a2b16412747b24e3047aef3
Javan Makhmali <javan@javan.us>
2016-10-03 10:54:31 -0400

    Handle autofocus in <webview>s

--- guest.js
+++ guest.js
@@ -2,3 +2,9 @@ const {ipcRenderer} = require('electron')
 const {claimDocumentFocus} = require('./document-focus')
 
 ipcRenderer.on('claim-document-focus', claimDocumentFocus)
+
+addEventListener('DOMContentLoaded', function() {
+  if (document.querySelector('[autofocus]')) {
+    ipcRenderer.sendToHost('autofocus')
+  }
+})
--- host.js
+++ host.js
@@ -1,6 +1,6 @@
 const {releaseDocumentFocus} = require('./document-focus')
 
-function giveWebviewFocus (element) {
+function giveWebviewDocumentFocus (element) {
   releaseDocumentFocus()
 
   requestAnimationFrame(function () {
@@ -12,10 +12,29 @@ function elementIsUnfocusedWebview (element) {
   return element.tagName === 'WEBVIEW' && !element.getWebContents().isFocused()
 }
 
+function webviewDidAutofocus (element) {
+  function didKeyDown (event) {
+    element.removeEventListener('keydown', didKeyDown, true)
+    giveWebviewDocumentFocus(element)
+  }
+
+  element.addEventListener('keydown', didKeyDown, true)
+}
+
+function handleAutofocus (element) {
+  element.addEventListener('ipc-message', function (event) {
+    if (event.channel === 'autofocus') {
+      element.focus()
+      webviewDidAutofocus(element)
+    }
+  })
+}
+
 function didMouseDown (event) {
   if (elementIsUnfocusedWebview(event.target)) {
-    giveWebviewFocus(event.target)
+    giveWebviewDocumentFocus(event.target)
   }
 }
 
 addEventListener('mousedown', didMouseDown, true)
+document.querySelectorAll('webview').forEach(handleAutofocus)
